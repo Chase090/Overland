2f5133d4c6608c30c2f07e6949e1edf5461420cf
Chase090 <carloyam21@icloud.com>
2021-02-22 05:04:26 -0800

    added user function login via google, testing status

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/auth/:palceholder/callback', to: 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
 end
